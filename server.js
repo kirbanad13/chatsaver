ededef8be0b69f046d1bbdb38dfd756989831d60
K0KCAKEP <112177395+K0KCAKEP@users.noreply.github.com>
2023-04-14 19:40:08 +0300

    saving chat after stream live

--- server.js
+++ server.js
@@ -1,27 +1,67 @@
 const WebSocket = require("ws");
 const fs = require("fs");
+const fetch = require("node-fetch");
 
-const chatSocket = new WebSocket("wss://chat.goodgame.ru/chat/websocket");
+//199261 k0kcakep
+//137757 Arrowwood
+const channelId = 137757;
 
-chatSocket.addEventListener("open", (event) => {
-  const message = {
-    type: "join",
-    data: {
-      channel_id: "137757",
-      hidden: false,
-    },
-  };
+checkStatus();
 
-  chatSocket.send(JSON.stringify(message));
-});
-
-chatSocket.onmessage = function (event) {
-  const message = JSON.parse(event.data);
-
-  if (message.type === "message") {
-    fs.appendFile("messages.json", JSON.stringify(message) + ",", (err) => {
-      if (err) throw err;
-      console.log("Message saved to file");
+function checkStatus() {
+  fetch(`https://goodgame.ru/api/getchannelstatus?id=${channelId}&fmt=json`)
+    .then((response) => response.json())
+    .then((data) => {
+      const status = data[channelId].status;
+      console.log(data[channelId].status);
+      if (status === "Live") {
+        saveChat();
+      } else {
+        setTimeout(checkStatus, 500);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      setTimeout(checkStatus, 500); // Check again in 5 seconds
     });
-  }
-};
+}
+
+function saveChat() {
+  const chatSocket = new WebSocket("wss://chat.goodgame.ru/chat/websocket");
+  const now = new Date();
+  const fileName = createFileName();
+
+  chatSocket.addEventListener("open", (event) => {
+    const message = {
+      type: "join",
+      data: {
+        channel_id: channelId,
+        hidden: false,
+      },
+    };
+
+    chatSocket.send(JSON.stringify(message));
+  });
+
+  chatSocket.onmessage = function (event) {
+    const message = JSON.parse(event.data);
+
+    if (message.type === "message" || message.type === "remove_message") {
+      fs.appendFile(fileName, JSON.stringify(message) + ",", (err) => {
+        console.log("Message saved to file");
+      });
+    }
+  };
+}
+
+function createFileName() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  const hour = String(now.getHours()).padStart(2, "0");
+  const minute = String(now.getMinutes()).padStart(2, "0");
+  const second = String(now.getSeconds()).padStart(2, "0");
+
+  return `${year}_${month}_${day}_${hour}_${minute}_${second}_messages.json`;
+}
